d6c9f14763ea717ef4e1b769c84b995a38da1795
droggokid <raduflorinmunteanu@gmail.com>
2024-05-12 13:37:44 +0200

    section v0.1

--- src/components/Section.jsx
+++ src/components/Section.jsx
@@ -7,8 +7,8 @@ function Section() {
     return (
         <div className='hero-container'>
             {/*<video src="/videos/video-2.mp4" autoPlay loop muted />*/}
-            <h1>ADVENTURE AWAITS</h1>
-            <p>What are you waiting for?</p>
+            <h1 className='Title'>Leverum (Karrierevejledning) & Noderum (Musikundervisning)</h1>
+            <p className='Slogan'>Til dig der har brug for at spille på andre strenge i dit liv</p>
             <div className='hero-btns'>
                 <Button className='btns' 
                 buttonStyle='btn--outline' 

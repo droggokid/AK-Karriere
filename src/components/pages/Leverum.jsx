3c0e99de60a9af16aabe1e8efbb41b4b462d7866
droggokid <raduflorinmunteanu@gmail.com>
2024-05-12 16:52:10 +0200

    section v0.2.5

---
+++ src/components/pages/Leverum.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import '../../App.css';
+
+
+export default function Leverum() {
+  return <h1 className='leverum'>LEVERUM</h1>;
+}
+
---
+++ src/components/pages/Noderum.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import '../../App.css';
+
+
+export default function Noderum() {
+  return <h1 className='noderum'>Noderum</h1>;
+}
+
